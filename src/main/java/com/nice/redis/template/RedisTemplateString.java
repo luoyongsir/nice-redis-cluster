77ede64c3a96de30e9654c01d56f189bf076e708
罗勇 <luoyong@13322ty.com>
2018-08-16 15:28:15 +0800

    fix

--- src/main/java/com/nice/redis/template/RedisTemplateString.java
+++ src/main/java/com/nice/redis/template/RedisTemplateString.java
@@ -1,11 +1,9 @@
 package com.nice.redis.template;
 
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
-import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.serializer.RedisSerializer;
-
-import java.util.Collections;
+import redis.clients.jedis.JedisCommands;
 
 /**
  * String序列化缓存对象
@@ -33,18 +31,6 @@ public class RedisTemplateString extends AbstractTemplate {
     private static final long DEFAULT_TRY_LOCK_TIME_OUT = 1000L;
 
     /**
-     * 拿锁的EVAL函数
-     */
-    private static final String LOCK_LUA_SCRIPT = "return redis.call('SET', KEYS[1], ARGV[1], 'NX', 'PX', ARGV[2]) ";
-    private static RedisScript<String> lockScript = new DefaultRedisScript<>(LOCK_LUA_SCRIPT, String.class);
-
-    /**
-     * 释放锁的EVAL函数
-     */
-    private static final String UNLOCK_LUA_SCRIPT = "if (redis.call('GET', KEYS[1]) == ARGV[1]) then return redis.call('DEL',KEYS[1]) else return 0 end";
-    private static RedisScript<String> unlockScript = new DefaultRedisScript<>(UNLOCK_LUA_SCRIPT, String.class);
-
-    /**
      * 尝试获取分布式锁
      *
      * @param lockKey      锁
@@ -56,9 +42,11 @@ public class RedisTemplateString extends AbstractTemplate {
         if (expireTimeMs == null || expireTimeMs <= 0L) {
             expireTimeMs = DEFAULT_LOCK_TIME_OUT;
         }
-        // 后面的三个参数分别对应了scriptLock字符串中的三个变量值，KEYS[1]，ARGV[1]，ARGV[2]，含义为锁的key，key对应的value，以及key的存在时间(单位毫秒)
-        String result = template.execute(lockScript, Collections.singletonList(lockKey), lockValue, expireTimeMs.toString());
-        // 返回“OK”代表拿到锁
+        final long expire = expireTimeMs;
+        String result = template.execute((RedisCallback<String>) con -> {
+            JedisCommands commands = (JedisCommands) con.getNativeConnection();
+            return commands.set(lockKey, lockValue, "NX", "PX", expire);
+        });
         return OK.equals(result);
     }
 
@@ -96,11 +84,10 @@ public class RedisTemplateString extends AbstractTemplate {
      * 释放分布式锁
      *
      * @param lockKey   锁
-     * @param lockValue 能区分请求的唯一标识
      * @return 是否释放成功
      */
-    public boolean unLock(String lockKey, String lockValue) {
-        Object result = template.execute(unlockScript, Collections.singletonList(lockKey), lockValue);
-        return ONE.equals(String.valueOf(result));
+    public boolean unLock(String lockKey) {
+        Long res = del(lockKey);
+        return ONE.equals(res);
     }
 }
